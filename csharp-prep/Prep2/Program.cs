31c460a9cdfb68e5be51bd17793696a398bbffeb
Your Name <you@example.com>
2024-01-23 14:17:18 +0100

    Added Code on Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,65 @@ class Program
     static void Main(string[] args)
     {
         // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        double grade = Convert.ToDouble(Console.ReadLine());
+        string letter;
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = (int)grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // Handle the exceptional cases of A+ and F+/F-
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter + sign);
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+    }
+}
+
     }
 }
