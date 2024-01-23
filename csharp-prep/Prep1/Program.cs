57840a0a4d55478e1c6976b9e70f23ced2b53466
Your Name <you@example.com>
2024-01-23 03:56:56 +0100

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+         // Prompt the First anme
+        Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();
+
+         // prompt the Last name
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+         // Display full name in the specified format
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
